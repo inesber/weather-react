6e9043a9d75d22fcdfa58af39c3c878fea037ebd
Inês Bernardino <icmbern@gmail.com>
2019-11-21 19:29:20 +0000

    Added Loader

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import "./App.css";
+import Loader from "react-loader-spinner";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello </h1>
+        <Loader type="Puff" color="#00BFFF" height={100} width={100} />
+        <a href="https://github.com/inesber/weather-react" target="_blank">
+          Open-Source Code
+        </a>{" "}
+        by Inês Bernardino
       </header>
-      <a href="https://github.com/inesber/weather-react" target="_blank">
-        Open-Source Code
-      </a>{" "}
-      by Inês Bernardino
     </div>
   );
 }
